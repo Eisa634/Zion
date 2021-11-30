c7e020c236906bfb0a0f5360ca7c0bb6c2efef9f
Illuminados <95315191+Illuminados@users.noreply.github.com>
2021-11-30 20:44:54 +0000

    Create guesser.lua

---
+++ guesser.lua
@@ -0,0 +1,13 @@
+local randomNumber1 = math.random(1, 5)
+local num1 = io.read()
+
+  print(randomNumber1)
+
+if (randomNumber1 == num1)
+    then print("correct")
+  end
+
+if (randomNumber1 ~= num1)
+    then print("incorrect")
+
+end
